26dae07117b7c800c1c1d0c4acb5d3877b464b33
Chris Rachlinski <chrisrachlinski@gmail.com>
2026-04-18 21:30:12 -0400

    Moving photo to separate repo

--- photo/tf/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "api_endpoint" {
-  description = "API Gateway endpoint URL"
-  value       = aws_apigatewayv2_stage.default.invoke_url
-}
-
-output "photo_bucket_name" {
-  description = "S3 bucket for photo uploads"
-  value       = aws_s3_bucket.photos.bucket
-}
